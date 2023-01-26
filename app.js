13e8a74248632704f900d24f2309ed107a9abaab
ymonsalvee <72816902+ymonsalvee@users.noreply.github.com>
2023-01-26 20:04:19 +0100

    some fixes in back still error connecting

--- app.js
+++ app.js
@@ -2,57 +2,27 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path"); //what is this?
 var cookieParser = require("cookie-parser"); //what is this?
-//var logger = require("morgan"); // what is this?
+var logger = require("morgan"); // what is this?
 
+var postRouter = require("./routes/posts");
 // var indexRouter = require("./routes/index");
 // var usersRouter = require("./routes/users"); //from express
 
-const db = require("../model/helper"); //use helper after set up
-
 var app = express();
-//var router = express.Router();
 
-// app.use(logger("dev"));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/", postRouter);
 // app.use("/", indexRouter);
 // app.use("/users", usersRouter);
-
-// app.get("/", (req, res) => {
-//   res.send("Welcome to the API");
-// });
-
-app.get("/", async (req, res) => {
-  try {
-    const response = await db("SELECT * FROM posts");
-    const posts = response.data;
-    res.send({ posts });
-  } catch (err) {
-    res.status(500).send(err);
-  }
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
-
-app.post("/", async (req, res) => {
-  const title = req.body.title;
-  const description = req.body.description;
-  //const createdAt = req.body.createdAt // this should get the timestamp and added to created_at
-  try {
-    await db(
-      `INSERT INTO posts (title, description) value ('${title}',('${description}')`
-    );
-    res.status(200).send({ message: "post created" });
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
 
 app.get;
 // // error handler
---
+++ routes/posts.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var router = express.Router();
+const db = require("../model/helper"); //use helper after set up
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+module.exports = router;
+
+router.get("/", (req, res) => {
+  res.send("Welcome to the API");
+});
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const response = await db("SELECT * FROM posts");
+//     const posts = response.data;
+//     res.send({ posts });
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
+
+// router.post("/", async (req, res) => {
+//   const title = req.body.title;
+//   const description = req.body.description;
+//   //const createdAt = req.body.createdAt // this should get the timestamp and added to created_at
+//   try {
+//     await db(
+//       `INSERT INTO posts (title, description) value ('${title}',('${description}')`
+//     );
+//     res.status(200).send({ message: "post created" });
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
