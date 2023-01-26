6c5e7a4c43dcd7fd1d4ba6fb1683db73710e1727
ymonsalvee <72816902+ymonsalvee@users.noreply.github.com>
2023-01-26 22:36:09 +0100

    frontend progress submitform components working, database migration success

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,31 @@ import CreatePostForm from "./components/CreatePostForm";
 import PostView from "./components/PostView";
 
 function App() {
+  // const [lists, setLists] = useState([
+  //   {
+  //     name: "Journal",
+  //     description: "here is where I will write what happens to me day to day",
+  //   },
+  //   {
+  //     name: "Random ideas",
+  //     description: "here is where I will write whatever comes to mind",
+  //   },
+  //   {
+  //     name: "Recipes",
+  //     description: "here is where I will write down any good recipe",
+  //   },
+  // ]);
+  // const[(posts, setPosts)] = useState([{}]);
   return (
     <div className="App">
       <h1> write it down</h1>
       {/* if there are no list */}
       <p>You have no lists yet, create one</p>
-
       <CreateListForm />
+      {/* onSubmit={(newList) => handleCreateList(newList)} */}
       <ListView />
       <CreatePostForm />
+      {/* onSubmit={(newPost) => handleCreatePost(newPost)} */}
       <PostView />
     </div>
   );
--- model/init.sql
+++ model/init.sql
@@ -1,46 +1,23 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS lists;
 DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS lists;
 
-
- CREATE TABLE users  (
-id INT AUTO_INCREMENT,
-name VARCHAR(20),
-PRIMARY KEY (id));
-
- CREATE TABLE lists  (
-id INT AUTO_INCREMENT,
-name VARCHAR(50),
-userID INT,
-PRIMARY KEY (id),
-FOREIGN KEY (userID) REFERENCES users (id)
+CREATE TABLE lists  (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50),
+  description  VARCHAR(100)
 );
 
- CREATE TABLE posts  (
-id INT AUTO_INCREMENT,
-title VARCHAR(100),
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-text VARCHAR(500),
-listID INT,
-imageID // not added yet 
-PRIMARY KEY (id),
-FOREIGN KEY (listID) REFERENCES lists (id)
+CREATE TABLE posts  (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(100),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+  text VARCHAR(500),
+  list_id INT NOT NULL
 );
 
---  CREATE TABLE images (
--- id INT AUTO_INCREMENT,
--- name VARCHAR(50),
--- Image BLOB
--- postID INT,
--- PRIMARY KEY (id),
--- FOREIGN KEY (postID) REFERENCES posts (id)
--- );
+ALTER TABLE posts
+ADD FOREIGN KEY (list_id) REFERENCES lists(id);
 
--- INSER INTO users (name) values ("CarlitaMaria")
+INSERT INTO lists(name) VALUES ('Pruebita');
+INSERT INTO posts(title, text, list_id) VALUES ('Prueba de la pruebita', 'Random', 1);
 
--- does this update my db in mysql? i changed it here since it made 
--- a mistake in the command line, it still shows the previous name, i got and 
--- error when trying to migrate
-
--- error Command failed with exit code 1.
--- info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
